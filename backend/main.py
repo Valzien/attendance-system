24f2d8d0335ed08c3d7287d99a7b72b16f5c37a0
Valzien <nugraharival1736@gmail.com>
2026-06-28 02:37:14 +0700

    Fix CORS for Cloudflare Pages

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,11 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://.*\.attendance-system-1uj\.pages\.dev",
+    allow_origins=[
+        "http://127.0.0.1:5173",
+        "http://localhost:5173",
+        "https://attendance-system-1uj.pages.dev",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
